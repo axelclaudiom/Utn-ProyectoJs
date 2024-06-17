64a850a665f65f2c552968b51bc833ff3e5988a2
Axel Claudio Melgar <axelclaudiom@gmail.com>
2024-06-17 02:34:16 -0300

    add: I show each character that has an image on a card and their data.

--- js/personajes.js
+++ js/personajes.js
@@ -2,7 +2,23 @@ fetch("https://hp-api.onrender.com/api/characters")
 .then(response => (
     response.json()
 )).then(data => {
-    data.forEach((personaje) =>
-        console.log(personaje)        
-    )
+    const $grid = document.querySelector(".grid");
+    data.forEach((personaje) => {
+        //console.log(personaje)
+        if (personaje.image) {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            console.log(personaje)
+            card.innerHTML = `
+                                <img src="${personaje.image}" alt="${personaje.name}">
+                                <div class="card-content">
+                                    <h3>${personaje.name}</h3>
+                                    <p><strong>Fe de vida:</strong> ${ (personaje.alive) ? `Vivo` : `Muerto` }</p>
+                                    <p><strong>Especie:</strong> ${personaje.species}</p>
+                                    <p><strong>Intérprete:</strong> ${personaje.actor}</p>
+                                </div>
+                            `;
+            $grid.appendChild(card);
+        }
+    })
 })
