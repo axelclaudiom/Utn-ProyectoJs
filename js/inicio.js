ba9ee99dcc84d89b6cd61251c3b0a176ff2dc63a
Axel Claudio Melgar <axelclaudiom@gmail.com>
2024-06-18 15:24:09 -0300

    add: dynamically rendering information about characters, houses and spells.

--- js/inicio.js
+++ js/inicio.js
@@ -2,19 +2,38 @@ const $buttonPersonajes = document.getElementById('personajesButton');
 
 $buttonPersonajes.addEventListener("click" , (e) => {
     e.preventDefault();
-    console.log("Personajes")
+    //console.log("Personajes")
+    const template = `
+    <h3>Personajes</h3>
+	<p>Descubre la historia, las características y las curiosidades de tus personajes favoritos, desde el valiente Harry Potter y sus amigos Hermione Granger y Ron Weasley, hasta los enigmáticos Severus Snape y Lord Voldemort. Cada personaje tiene su propia magia y secretos que te invitamos a conocer.</p>
+`
+	const data = document.getElementById('data')
+	data.innerHTML = template
 })
 
 const $buttonCasas = document.getElementById('casasButton');
 
 $buttonCasas.addEventListener("click" , (e) => {
     e.preventDefault();
-    console.log("Casas")
+    //console.log("Casas")
+    const template = `
+    <h3>Casas</h3>
+	<p>Adéntrate en el espíritu de las cuatro casas de Hogwarts: Gryffindor, Hufflepuff, Ravenclaw y Slytherin. Aprende sobre sus fundadores, sus valores y las características que definen a sus miembros. ¿A cuál casa perteneces tú?.</p>
+`
+	const data = document.getElementById('data')
+	data.innerHTML = template
 })
 
 const $buttonHechizos = document.getElementById('hechizosButton');
 
 $buttonHechizos.addEventListener("click" , (e) => {
     e.preventDefault();
-    console.log("Hechizos")
+    //console.log("Hechizos")
+    const template = `
+    <h3>Hechizos</h3>
+	<p>La magia está en el corazón de este mundo, y los hechizos son la herramienta principal de todo mago y bruja. Aquí encontrarás una guía completa de los hechizos más importantes, desde el encantamiento más sencillo hasta los hechizos más complejos y poderosos.
+Prepárate para un viaje mágico lleno de descubrimientos y maravillas. ¡Bienvenido a nuestra comunidad de amantes de Harry Potter!</p>
+`
+	const data = document.getElementById('data')
+	data.innerHTML = template
 })
